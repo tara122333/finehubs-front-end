6a7670a8743c8ddbcb4d6ba970bb3b67e4becfdd
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2023-01-09 14:55:43 +0530

    stablish code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 // import {Routes,Route} from 'react-router-dom'
 
-// import Home from "./Page/Home";
-import Temp from "./Page/Temp";
+import Home from "./Page/Home";
 
 
 // HOC Files
@@ -11,14 +10,7 @@ import DefaultHOC from "./HOC/Home.HOC";
 function App() {
   return (
     <div>
-      {/* <h1>This is heading tag</h1> */}
-      {/* <Home /> */}
-       {/* <Routes>
-        <Route path="/home" exact  element={<Home/>}/>
-        <Route path="/temp" exact element={<Temp/>}/>
-      </Routes>  */}
-      <DefaultHOC path="/" exact element={Temp} />
-      <DefaultHOC path="/home" exact element={Temp} />
+      <DefaultHOC path="/" exact element={Home} />
     </div>
   );
 }
